008f237473cc4fcfb591e20bf4ae07a0df1745c5
Dzmitry Suhaka <dzsuhaka@gmail.com>
2018-10-16 16:18:11 +0300

    added fields, switched from list to table

--- MyHTTPServer.py
+++ MyHTTPServer.py
@@ -12,6 +12,7 @@ import cgi
 import sys
 import shutil
 import mimetypes
+import time
 try:
     from cStringIO import StringIO
 except ImportError:
@@ -40,9 +41,11 @@ class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         f = StringIO()
         displaypath = cgi.escape(urllib.unquote(self.path))
         f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<html>\n<head>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<style>th { padding: 0px 10px 0px 10px; } table { font-family: courier new; }</style>\n</head>\n")
         f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
-        f.write("<hr>\n<ul>\n")
+        f.write("<hr>\n<table>\n")
+        f.write("<tr><th>Name</th><th>Size</th><th>Modified</th></tr>")
         for name in list:
             fullname = os.path.join(path, name)
             displayname = linkname = name
@@ -50,12 +53,15 @@ class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             if os.path.isdir(fullname):
                 displayname = name + "/"
                 linkname = name + "/"
+                size = ''
+            else:
+                size = os.stat(fullname).st_size
             if os.path.islink(fullname):
                 displayname = name + "@"
                 # Note: a link to a directory displays with @ and links with /
-            f.write('<li><a href="%s">%s</a>\n'
-                    % (urllib.quote(linkname), cgi.escape(displayname)))
-        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+            modified = time.ctime(os.stat(fullname).st_mtime)
+            f.write('<tr><th align="left"><a href="{}">{}</a></th><th align="right">{}</th><th>{}</th></tr>'.format(urllib.quote(linkname), cgi.escape(displayname), size, modified))
+        f.write("</table>\n<hr>\n</body>\n</html>\n")
         length = f.tell()
         f.seek(0)
         self.send_response(200)
